8d49df2f2b4f42e7b36cbb291d05b68d8ef454fd
jawwad-ali <alijawwad001@gmail.com>
2022-02-11 00:03:18 +0500

    navvigation bar complete

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Article } from "./article";
+export { default as Brand } from "./brand";
+export { default as Feature } from "./feature";
+export { default as Navbar } from "./navbar";
+export { default as Cta } from "./cta"; 
